b2b72a41ef9b1ee83f438ccb167e03b87b254292
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-05-05 13:20:24 +0800

    Update NAStxtToDB.py
    # Load text files to MySQL DB

--- scripts/NAStxtToDB.py
+++ scripts/NAStxtToDB.py
@@ -7,6 +7,7 @@ Created on Thu May  4 15:40:48 2017
 # Import packages and libraries
 import datetime
 import glob
+import re
 
 # Search for files and paths for current date
 root = '/mnt/nas/CMS_Data'
@@ -14,3 +15,10 @@ year = datetime.date.today().strftime("%Y")
 month = datetime.date.today().strftime("%Y-%m")
 day = datetime.date.today().strftime("%Y-%m-%d")
 files = glob.glob("%s/StationsData_*/*/Converted to TXT/%s/%s/*%s.txt" % (root, year, month, day))
+
+# Load text files to MySQL DB
+for file in files:
+    print(file)
+    station = re.findall('\[([0-9]+)\]', file)[0]
+    table = 'SIN' + station
+    print(table)
