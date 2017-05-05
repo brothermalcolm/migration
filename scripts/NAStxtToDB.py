2d8a7974c44280839089bf683a66c83f54d54c8e
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-05-05 13:18:44 +0800

    Update NAStxtToDB.py
    # Search for files and paths for current date

--- scripts/NAStxtToDB.py
+++ scripts/NAStxtToDB.py
@@ -6,9 +6,11 @@ Created on Thu May  4 15:40:48 2017
 """
 # Import packages and libraries
 import datetime
+import glob
 
 # Search for files and paths for current date
 root = '/mnt/nas/CMS_Data'
 year = datetime.date.today().strftime("%Y")
 month = datetime.date.today().strftime("%Y-%m")
 day = datetime.date.today().strftime("%Y-%m-%d")
+files = glob.glob("%s/StationsData_*/*/Converted to TXT/%s/%s/*%s.txt" % (root, year, month, day))
